50839b65bfd7409a1f8c7550045939e1c5f7a3a0
abhinav0343 <abhinavgolla2007@gmail.com>
2024-09-18 23:03:47 +0530

    Update 44.c

--- 44.c
+++ 44.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
 float u,charge,amount;
@@ -11,15 +12,15 @@ charge=0.5*u;
 }
 else if(u>200&&u<=400)
 {
-charge=100+(0.65*u);
+charge=100+(0.65*(u-200));
 }
 else if(u>400&&u<=600)
 {
-charge=230+(0.8*u);
+charge=230+(0.8*(u-400));
 }
 else
 {
-charge=425+(1.25*u);
+charge=425+(1.25*(u-600));
 }
 printf("amount=%f\n",charge);
 return 0;
