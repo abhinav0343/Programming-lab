8d965ca225505e2c111ce73bc2be8d8b01651eb2
abhinav0343 <abhinavgolla2007@gmail.com>
2024-09-30 22:38:08 +0530

    Add files via upload

---
+++ 64.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int a[3][3],i,j,sr,sc;
+printf("entet the matrix elements\n");
+for(i=0;i<3;i++)
+ { 
+   for(j=0;j<3;j++)
+    {
+      scanf("%d",&a[i][j]);
+    }
+ }
+ printf("the matrix is\n");
+   for(i=0;i<3;i++)
+ { 
+   for(j=0;j<3;j++)
+    {
+      printf("%d\t",a[i][j]);
+    }
+    printf("\n");
+ }
+for(i=0;i<3;i++)
+ { 
+  sr=sc=0;
+   for(j=0;j<3;j++)
+    {
+      sr = sr + a[i][j];
+      sc = sc + a[j][i];
+    }
+    printf(" sr = %d, sc = %d \n ",sr,sc);
+ }
+ }
