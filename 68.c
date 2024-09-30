3672355da6ab18b906185ca3a0d39f1b82d08ef7
abhinav0343 <abhinavgolla2007@gmail.com>
2024-09-30 23:56:36 +0530

    Update 68.c

--- 68.c
+++ 68.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int a[3][3],i,j,b[3][3],c[3][3];
+int a[3][3],i,j,b[3][2],c[3][2],k,sum;
 printf("entet the matrix elements of A\n");
 for(i=0;i<3;i++)
  { 
@@ -10,7 +10,7 @@ for(i=0;i<3;i++)
       scanf("%d",&a[i][j]);
     }
  }
- printf("the matrix A is\n");
+ printf("\nthe matrix A is\n");
    for(i=0;i<3;i++)
  { 
    for(j=0;j<3;j++)
@@ -19,31 +19,37 @@ for(i=0;i<3;i++)
     }
     printf("\n");
  }
- printf("enter the matrix elements of B\n");
+ printf("\nenter the matrix elements of B\n");
  for(i=0;i<3;i++)
  { 
-   for(j=0;j<3;j++)
+   for(j=0;j<2;j++)
     {
       scanf("%d",&b[i][j]);
     }
  }
- printf("the matrix B is\n");
+ printf("\nthe matrix B is\n");
    for(i=0;i<3;i++)
  { 
-   for(j=0;j<3;j++)
+   for(j=0;j<2;j++)
     {
       printf("%d\t",b[i][j]);
     }
     printf("\n");
  }
- printf("multiplication of a and b matices are\n");
-  for(i=0;i<3;i++)
- { 
-   for(j=0;j<3;j++)
-    {
-      c[i][j] = a[i][j] * b[i][j];
-      printf("%d\t",c[i][j]);
-    }
-    printf("\n");
- }
- }
+ printf("\nmultiplication of the two matrices are\n");
+ for(i=0;i<3;i++)
+  {
+    for(j=0;j<2;j++)
+     {
+     sum=0;
+       for(k=0;k<3;k++)
+        {
+          sum = sum + a[i][k]*b[k][j];
+          c[i][j] = sum;
+        }
+          printf("%d\t",c[i][j]);
+        
+     }
+        printf("\n");
+  } 
+  } 
