fd39bcc40be4a741518398118315bcf954d30124
abhinav0343 <abhinavgolla2007@gmail.com>
2024-10-17 00:14:19 +0530

    Update 53.c

--- 53.c
+++ 53.c
@@ -1,36 +1,32 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,n,k;
-	printf("enter the number");
-	scanf("%d",&n);
-	for(i=1;i<=2*n-1;i++)
-	{
-	    for(j=1;j<=2*(n-i);j++)
-		{
-			printf(" ");
-		}
-		for(j=1;j<=2*i-1;j++)
-		{
-			printf("* ",j);
-		}
-		printf("\n");
-     if(i>n-1)
-     {
-     break;
-     }
-	}
-	for(i=n-1;i>0;i--)
-       {
-         for(j=1;j<=2*(n-i);j++)
-	           {
-	               printf(" ");
-	           }
-	        for(j=1;j<=2*i-1;j++)
-	        {
-	            printf("* ");
-	        }
-	        printf("\n");
-	   }
-	return 0;
-}    
+int n,i,j,s;
+printf("enter the number of rows\n");
+scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(s=1;s<=(n-i);s++)
+    {
+      printf(" ");
+    }
+         for(j=1;j<=2*i-1;j++)
+         {
+           printf("*"); 
+         }
+      printf("\n");
+  }
+  for(i=n-1;i>=1;i--)
+  {
+    for(s=1;s<=(n-i);s++)
+    {
+      printf(" ");
+    }
+         for(j=1;j<=2*i-1;j++)
+         {
+           printf("*"); 
+         }
+      printf("\n");
+  }
+ return 0;
+}
