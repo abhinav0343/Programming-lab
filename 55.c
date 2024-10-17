4b4fe2bf0e125dab33c413ab109302396e699941
abhinav0343 <abhinavgolla2007@gmail.com>
2024-10-17 08:42:57 +0530

    Update 55.c

--- 55.c
+++ 55.c
@@ -1,25 +1,29 @@
+
 #include<stdio.h>
 int main()
 {
-   int i,even=0,odd=0,arr[10];
+   int i,even=0,odd=0,n;
+   printf("enter the size of array\n");
+   scanf("%d",&n);
+   int arr[n];
    printf("to count no:of  even and odd numebrs\n");
    printf("enter the numbers\n");
-   for(i=0;i<10;i++)
+   for(i=0;i<n;i++)
    {
    scanf("%d",&arr[i]);
    }
-   for(i=0;i<10;i++)
+   for(i=0;i<n;i++)
    {
       if(arr[i] % 2 == 0)
       {
-      even=even+1;
+      even++;
       }
       else
       {
-      odd=odd+1;
+      odd++;
       }
    }
    printf("no:of even numbers are %d\n",even);
    printf("no:of odd numbers are %d\n",odd);
    return 0;
-   }
+}
