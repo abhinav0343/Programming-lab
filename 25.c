48a3b3773c55975e3bbd428a8b048129ab1eb83e
abhinav0343 <abhinavgolla2007@gmail.com>
2024-11-28 13:12:40 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,33 @@
+#include<stdio.h>//program to print table of a number
+
+int main(){
+
+int n,i;
+
+
+
+    printf("enter a number n:");
+
+
+
+    scanf("%d",&n);
+
+
+
+    printf("the table of n is : \n");
+
+
+
+for(i=1;i<=10;i++){
+
+
+
+    printf("%d \n",n*i);
+
+
+
+}
+
+
+
+return 0;}
