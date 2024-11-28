57763f77079c065f5e5646f4362ca67b13cfa227
abhinav0343 <abhinavgolla2007@gmail.com>
2024-11-28 14:36:13 +0530

    Update 100.c

--- 100.c
+++ 100.c
@@ -20,7 +20,7 @@ int main()
         scanf("%d%s%d",&a[i].roll,&a[i].s,&a[i].t);
     }
     FILE *fp;
-    fp=fopen("LNMIITSTUDENT.txt","w");
+    fp=fopen("LNMIITSTUDENT.JAVA","w");
     if(fp==NULL)
     {
         printf("error opening file for writing");
