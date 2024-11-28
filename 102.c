77595c6599f9d230b12b6186f01cb2c7d9c07545
abhinav0343 <abhinavgolla2007@gmail.com>
2024-11-28 13:16:47 +0530

    Create 102.c

---
+++ 102.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    FILE *file;
+    int c_count = 0, w_count = 1, l_count = 1;
+    char c;
+    // reading the sample text
+    file = fopen("/Users/starktek/Desktop/VS CODE/sample.txt", "r");
+    //foef return 0 if a valid char is present and -1 if NULL
+    while (c != EOF)
+    {
+        c = getc(file);
+        if (c == ' ')
+        {
+            w_count++;
+        }
+        if (c == '\n')
+        {
+            w_count++;
+            l_count++;
+        }
+        if (isalpha(c))
+        {
+            c_count++;
+        }
+    }
+
+    printf("number of alphabets: %d\n", c_count);
+    printf("number of words: %d\n", w_count);
+    printf("number of lines: %d\n", l_count);
+    fclose(file);
+    return 0;
+}
